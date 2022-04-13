bd54e7f230bccc155e81d42ba053debbad32c6d9
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-04-13 01:24:29 -0500

    feat: Added migrated to construct relationships

--- db/migrate/20220413053855_add_relationships.rb
+++ db/migrate/20220413053855_add_relationships.rb
@@ -1,9 +1,9 @@
 class AddRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key(:albums, :artists)
-    add_foreign_key(:songs, :albums)
-    add_foreign_key(:album_orders, :albums)
-    add_foreign_key(:album_orders, :orders)
-    add_foreign_key(:orders, :customers)
+    add_reference(:albums, :artists, foreign_key: true)
+    add_reference(:songs, :albums, foreign_key: true)
+    add_reference(:album_orders, :albums, foreign_key: true)
+    add_reference(:album_orders, :orders, foreign_key: true)
+    add_reference(:orders, :customers, foreign_key: true)
   end
 end
