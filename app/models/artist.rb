8707bf628113a60ddec8c9a3ffa37c4ed66af8fd
Frank Cano <frank.cano@utec.edu.pe>
2022-04-14 17:36:35 -0500

    feat/validations artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,22 @@
 class Artist < ApplicationRecord
+  has_many :albums
+  validates :name, presence: true
+  validate :validate_age
+  validate :death
+
+  def validate_age
+    if birth_date.present? &&  birth_date > Date.today
+      errors.add(:birth_date, 'birth date cannot be in the future')
+    end
+  end
+
+  def death
+    if death_date.present?
+      if birth_date.blank?
+        errors.add(:death_date, 'birth date do not exist')
+      elsif birth_date > death_date
+        errors.add(:death_date, 'death date is after the birth date')
+      end
+    end
+  end
 end
