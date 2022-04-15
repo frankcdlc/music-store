79a34899d6dfca97cbbd456f3184c888086ea169
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-04-14 21:43:04 -0500

    Fix(database): Added all FK fields to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_14_214449) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_15_024059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "album_orders", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "album_id", null: false
+    t.bigint "order_id", null: false
+    t.index ["album_id"], name: "index_album_orders_on_album_id"
+    t.index ["order_id"], name: "index_album_orders_on_order_id"
   end
 
   create_table "albums", force: :cascade do |t|
@@ -25,6 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_214449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "duration"
+    t.bigint "artist_id", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -53,6 +59,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_214449) do
     t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "customer_id", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
   end
 
   create_table "songs", force: :cascade do |t|
@@ -60,6 +68,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_14_214449) do
     t.integer "duration"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "album_id", null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
+  add_foreign_key "album_orders", "albums"
+  add_foreign_key "album_orders", "orders"
+  add_foreign_key "albums", "artists"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "songs", "albums"
 end
