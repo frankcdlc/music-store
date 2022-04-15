a05e4991d192b54db2ad80b863f0a9520b10834c
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-04-14 21:10:03 -0500

    Feat(1) Added seed to Artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+# Create 10 artists.
+
+10.times do |n|
+    birth_date = Faker::Date.between(from: 120.year.ago, to:18.year.ago)
+    artist = Artist.new(
+      name: Faker::Name.first_name, 
+      nationality: Faker::Nation.nationality, 
+      birth_date: birth_date,
+      death_date: Faker::Date.between(from: birth_date, to: Date.today),
+      biography: Faker::Lorem.sentence(word_count: n * 2)
+    )
+    if artist.save
+        puts "Artist#{n+1} created successfully"
+    else
+        puts "Artist#{n+1} #{artist.errors.full_messages.join(", ")}"
+    end
+end
