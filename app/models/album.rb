25a4da78d99620033b0ca81bb89eec091ea2e5e0
Frank Cano <frank.cano@utec.edu.pe>
2022-04-15 13:42:43 -0500

    ADD: feat/album validations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,7 @@
 class Album < ApplicationRecord
+  belongs_to :artist
+  has_many :songs
+  validates :artist, presence: true
+  validates :name, presence: true
+  validates :price, comparison: { greater_than: 0 }
 end
