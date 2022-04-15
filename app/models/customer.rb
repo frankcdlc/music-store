c15d36d19dc9f828c5fa9a8c2313ddeed6a01c5e
Frank Cano <frank.cano@utec.edu.pe>
2022-04-15 13:58:31 -0500

    ADD: customer-validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  has_many :orders
+  validates :name presence: true, uniqueness: true, on: :create
+  validates :email, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :password, presence: true, length: {minimum: 6}, format: { with: /.*[0-9].*/, on: :create}
 end
