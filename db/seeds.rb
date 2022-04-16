34ef29fb73eb986a49dcc0040c1967110526319e
tinyhosmarcos <yhostin.ollachica@ucsp.edu.pe>
2022-04-15 22:50:20 -0500

    fix:merged problems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-require "faker"
+require 'faker'
 # Create 10 artists.
 10.times do |n|
-  birth_date = Faker::Date.between(from: 120.years.ago, to: 18.years.ago)
-  artist = Artist.new(
-    name: Faker::Name.first_name,
-    nationality: Faker::Nation.nationality,
-    birth_date:,
-    death_date: Faker::Date.between(from: birth_date, to: Date.today),
-    biography: Faker::Lorem.sentence(word_count: n * 2)
-  )
-  if artist.save
-    Rails.logger.debug { "Artist#{n + 1} created successfully" }
-  else
-    Rails.logger.debug { "Artist#{n + 1} #{artist.errors.full_messages.join(', ')}" }
-  end
+    birth_date = Faker::Date.between(from: 120.year.ago, to:18.year.ago)
+    artist = Artist.new(
+      name: Faker::Name.first_name, 
+      nationality: Faker::Nation.nationality, 
+      birth_date: birth_date,
+      death_date: Faker::Date.between(from: birth_date, to: Date.today),
+      biography: Faker::Lorem.sentence(word_count: n * 2)
+    )
+    if artist.save
+        puts "Artist#{n+1} created successfully"
+    else
+        puts "Artist#{n+1} #{artist.errors.full_messages.join(", ")}"
+    end
 end
 
 # ALBUM Create between 2 to 6 albums for each artist. -> deberia estar adentro de la anterior iteracion?
@@ -28,17 +28,18 @@ artists = Artist.all # creado para guardar las instancais
 
 artists.each do |artist|
   4.times do |n|
-    album = artist.albums.new(
+    album=artist.albums.new(
       name: Faker::Music.album,
-      price: Faker::Number.decimal(l_digits: 2) # deberia ser con decimnales??????
+      price: Faker::Number.decimal(l_digits: 2)# deberia ser con decimnales??????
     )
     if album.save
-      Rails.logger.debug { "Album#{n + 1} created successfully" }
+      puts "Album#{n+1} created successfully"
     else
-      Rails.logger.debug { "Album#{n + 1} #{album.errors.full_messages.join(', ')}" }
+      puts "Album#{n+1} #{album.errors.full_messages.join(", ")}"
     end
   end
 end
+
 
 # SONG # Create between 4 and 10 songs for each album, necesitro la instancia de cada album
 albums = Album.all
@@ -47,12 +48,12 @@ albums.each do |album|
   rand(4..10).times.each do |n|
     song = album.songs.new(
       name: Faker::Music::GratefulDead.song,
-      duration: Faker::Number.between(from: 120, to: 300) # en segundos
+      duration: Faker::Number.between(from: 120, to: 300), # en segundos 
     )
     if song.save
-      Rails.logger.debug { "Song#{n + 1} created successfully" }
+      puts "Song#{n+1} created successfully"
     else
-      Rails.logger.debug { "Song#{n + 1} #{song.errors.full_messages.join(', ')}" }
+      puts "Song#{n+1} #{song.errors.full_messages.join(", ")}"
     end
   end
 end
@@ -60,21 +61,22 @@ end
 # Agregar duracion a un album
 
 albums.each do |album|
-  songs = Song.where(album_id: album.id)
+  songs = Song.where(album_id:album.id)
   duration = 0
   songs.each { |song| duration += song.duration }
   album.duration = duration
   if album.save
-    Rails.logger.debug { "Album#{album.id} update successfully" }
+    puts "Album#{album.id} update successfully"
   else
-    Rails.logger.debug { "Album#{album.id} #{album.errors.full_messages.join(', ')}" }
+    puts "Album#{album.id} #{album.errors.full_messages.join(", ")}"
   end
 end
+
 
 # #  COSTUMER # Create 15 customers.
 15.times do |n|
   costumer = Customer.new(
-    name: Faker::Name.first_name,
+    name: Faker::Name.first_name, 
     email: Faker::Internet.free_email, # yahoo hotmail gmail
     # email: Faker::Internet.email(domain: 'hotmail.com') #=> "alice@example.name"
     password: Faker::Alphanumeric.alphanumeric(number: 10),
@@ -89,15 +91,16 @@ end
 
 
 # # ORDER # Create between 1 and 5 orders for each customer. 
-sorted_customer = Customer.all.shuffle
+customers = Customer.all
 
-30.times do |n| 
-  order = Order.new(
-    date: Faker::Date.between(from: 120.year.ago, to: Date.today), # TRHIS CAN BE INPROVED
-    total: Faker::Number.decimal(l_digits: 2), #=> 11.88 esto debe ser un decimal-> puede denpender de albumn order y album price but idk how
-    # customer_id: Customer.all.find(((n/2)+1)).id
-    customer_id: sorted_customer[(n/2)].id
-  )
+customers.each do |customer|
+  rand(1..5).times.each do |n|
+    order = customer.orders.new(
+      date: Faker::Date.between(from: 120.year.ago, to: Date.today), # TRHIS CAN BE INPROVED
+      total: Faker::Number.decimal(l_digits: 2), #=> 11.88 esto debe ser un decimal-> puede denpender de albumn order y album price but idk how
+      
+    )
+  end
   if order.save
     puts "Order#{n+1} created successfully"
   else
